14e870276cca3c68475650120a9df38dcd1d4981
unknown <nitesh.yuvmedia@gmail.com>
2024-04-11 19:01:13 +0530

    First Commit of Auto Spot India

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+#--------------------------------------------------🙏🔱ॐ नमः शिवाय🔱🙏-------------------------------------
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
